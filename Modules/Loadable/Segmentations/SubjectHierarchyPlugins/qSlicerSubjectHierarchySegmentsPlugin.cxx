4e1d5de5572faade3e74e4833cf777e1a99eec75
Hans Johnson <hans-johnson@uiowa.edu>
2025-07-03 07:20:32 -0500

    ENH: Enable Qt auto-tools in preparation for Qt6 compatibility
    Preparing for Qt6 where the CMakeified auto-tooling
    is preferred over the explicit wrapping.

--- Modules/Loadable/Segmentations/SubjectHierarchyPlugins/qSlicerSubjectHierarchySegmentsPlugin.cxx
+++ Modules/Loadable/Segmentations/SubjectHierarchyPlugins/qSlicerSubjectHierarchySegmentsPlugin.cxx
@@ -1171,3 +1171,5 @@ void qSlicerSubjectHierarchySegmentsPlugin::setOpacityForCurrentItem(double opac
 
   displayNode->SetSegmentOpacity(segmentId, opacity);
 }
+
+#include "qSlicerSubjectHierarchySegmentsPlugin.moc"
--- Modules/Loadable/SubjectHierarchy/Widgets/qSlicerSubjectHierarchyCloneNodePlugin.cxx
+++ Modules/Loadable/SubjectHierarchy/Widgets/qSlicerSubjectHierarchyCloneNodePlugin.cxx
@@ -174,3 +174,5 @@ const QString qSlicerSubjectHierarchyCloneNodePlugin::getCloneNodeNamePostfix()
 {
   return QString(vtkSlicerSubjectHierarchyModuleLogic::CLONED_NODE_NAME_POSTFIX);
 }
+
+#include "qSlicerSubjectHierarchyCloneNodePlugin.moc"
